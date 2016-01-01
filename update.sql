66c8a866e1542bafc419077d4d96f1f489907b37
mhlx <admin@qyh.me>
2016-01-01 14:18:58 +0800

    修改test账户密码为123456

--- update.sql
+++ update.sql
@@ -1,3 +1,5 @@
+UPDATE blog_user SET user_password = '$2a$10$eJ8pM6Zl9tltoO/aHu9FFuUI6RIBoJmC8q740jajSj1X.Wt0JDowq' WHERE user_name = 'test';
+
 ALTER TABLE blog ADD COLUMN recommend BOOLEAN NOT NULL DEFAULT FALSE;
 ALTER TABLE blog_file ADD COLUMN cover INT ;
 ALTER TABLE blog_file ADD FOREIGN KEY(cover) REFERENCES blog_file(id);
