a8576656dce6aad44a21d4302358455edb329836
mhlx <admin@qyh.me>
2016-02-01 23:11:58 +0800

    1

--- src/main/webapp/static/common/common.js
+++ src/main/webapp/static/common/common.js
@@ -1,216 +1,243 @@
-String.prototype.endWith=function(str){     
-  var reg=new RegExp(str+"$");     
-  return reg.test(this);        
+String.prototype.endWith = function(str) {
+	var reg = new RegExp(str + "$");
+	return reg.test(this);
 };
 
-function getResizeUrl(url,size){
-	if(size && size != null){
+function getResizeUrl(url, size) {
+	if (size && size != null) {
 		var ext = url.split('.').pop();
-		var name = url.substring(0,url.indexOf(ext)-1);
+		var name = url.substring(0, url.indexOf(ext) - 1);
 		url = name + "_" + size + "." + ext;
 	}
 	return url;
 }
 
-Date.prototype.pattern=function(fmt) {         
-    var o = {         
-    "M+" : this.getMonth()+1, //月份         
-    "d+" : this.getDate(), //日         
-    "h+" : this.getHours()%12 == 0 ? 12 : this.getHours()%12, //小时         
-    "H+" : this.getHours(), //小时         
-    "m+" : this.getMinutes(), //分         
-    "s+" : this.getSeconds(), //秒         
-    "q+" : Math.floor((this.getMonth()+3)/3), //季度         
-    "S" : this.getMilliseconds() //毫秒         
-    };         
-    var week = {         
-    "0" : "/u65e5",         
-    "1" : "/u4e00",         
-    "2" : "/u4e8c",         
-    "3" : "/u4e09",         
-    "4" : "/u56db",         
-    "5" : "/u4e94",         
-    "6" : "/u516d"        
-    };         
-    if(/(y+)/.test(fmt)){         
-        fmt=fmt.replace(RegExp.$1, (this.getFullYear()+"").substr(4 - RegExp.$1.length));         
-    }         
-    if(/(E+)/.test(fmt)){         
-        fmt=fmt.replace(RegExp.$1, ((RegExp.$1.length>1) ? (RegExp.$1.length>2 ? "/u661f/u671f" : "/u5468") : "")+week[this.getDay()+""]);         
-    }         
-    for(var k in o){         
-        if(new RegExp("("+ k +")").test(fmt)){         
-            fmt = fmt.replace(RegExp.$1, (RegExp.$1.length==1) ? (o[k]) : (("00"+ o[k]).substr((""+ o[k]).length)));         
-        }         
-    }         
-    return fmt;         
-};     
+Date.prototype.pattern = function(fmt) {
+	var o = {
+		"M+" : this.getMonth() + 1, // 月份
+		"d+" : this.getDate(), // 日
+		"h+" : this.getHours() % 12 == 0 ? 12 : this.getHours() % 12, // 小时
+		"H+" : this.getHours(), // 小时
+		"m+" : this.getMinutes(), // 分
+		"s+" : this.getSeconds(), // 秒
+		"q+" : Math.floor((this.getMonth() + 3) / 3), // 季度
+		"S" : this.getMilliseconds()
+	// 毫秒
+	};
+	var week = {
+		"0" : "/u65e5",
+		"1" : "/u4e00",
+		"2" : "/u4e8c",
+		"3" : "/u4e09",
+		"4" : "/u56db",
+		"5" : "/u4e94",
+		"6" : "/u516d"
+	};
+	if (/(y+)/.test(fmt)) {
+		fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "")
+				.substr(4 - RegExp.$1.length));
+	}
+	if (/(E+)/.test(fmt)) {
+		fmt = fmt
+				.replace(
+						RegExp.$1,
+						((RegExp.$1.length > 1) ? (RegExp.$1.length > 2 ? "/u661f/u671f"
+								: "/u5468")
+								: "")
+								+ week[this.getDay() + ""]);
+	}
+	for ( var k in o) {
+		if (new RegExp("(" + k + ")").test(fmt)) {
+			fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k])
+					: (("00" + o[k]).substr(("" + o[k]).length)));
+		}
+	}
+	return fmt;
+};
 
-function getFirstDay(date){
-	var _date = new Date(date.getFullYear(),date.getMonth(),1);
+function getFirstDay(date) {
+	var _date = new Date(date.getFullYear(), date.getMonth(), 1);
 	return _date;
 }
 
-function getNextDay(date){
-	var _date = new Date(date.getFullYear(),date.getMonth(),date.getDate()+1);
+function getNextDay(date) {
+	var _date = new Date(date.getFullYear(), date.getMonth(),
+			date.getDate() + 1);
 	return _date;
 }
 
-function getFirstDayOfNextMonth(date){
-	var _date = new Date(date.getFullYear(),date.getMonth()+1,1);
+function getFirstDayOfNextMonth(date) {
+	var _date = new Date(date.getFullYear(), date.getMonth() + 1, 1);
 	return _date;
 }
 
 var isLogin = $("#isLogin").val() == "true";
 var staticSourcePrefix = getStaticResoucePrefix(0);
-function getStaticResoucePrefix(i){
+function getStaticResoucePrefix(i) {
 	var len = $(".staticSourcePrefix").length;
-	if(i > len-1 || i<0){
+	if (i > len - 1 || i < 0) {
 		return $(".staticSourcePrefix").eq(0).val();
 	}
 	return $(".staticSourcePrefix").eq(i).val();
 }
 var isGetToReadMessageCount = isLogin;
 
-function getToReadMessageCount(){
-	if(isGetToReadMessageCount){
+function getToReadMessageCount() {
+	if (isGetToReadMessageCount) {
 		$.ajax({
-			type: "GET",
-			url: contextPath + "/my/message/receive/getToReadMessageCount?sources=1",
-			dataType: "json",
-			success: function(data){
-				if(data.success){
+			type : "GET",
+			url : contextPath
+					+ "/my/message/receive/getToReadMessageCount?sources=1",
+			dataType : "json",
+			success : function(data) {
+				if (data.success) {
 					$("#toReadMessageBadge").html(data.result);
-				}else{
+				} else {
 					isGetToReadMessageCount = false;
 				}
-	         },
-	         error:function (request) {  
-	        	isGetToReadMessageCount = false;
-	         }
+			},
+			error : function(request) {
+				isGetToReadMessageCount = false;
+			}
 		});
 	}
 }
 
 var hasWebP = (function() {
-    var images = {
-        basic: "data:image/webp;base64,UklGRjIAAABXRUJQVlA4ICYAAACyAgCdASoCAAEALmk0mk0iIiIiIgBoSygABc6zbAAA/v56QAAAAA==",
-        lossless: "data:image/webp;base64,UklGRh4AAABXRUJQVlA4TBEAAAAvAQAAAAfQ//73v/+BiOh/AAA="
-    };
-    
-    return function(feature) {
-        var deferred = $.Deferred();
-        
-        $("<img>").on("load", function() {
-            if(this.width === 2 && this.height === 1) {
-                deferred.resolve();
-            } else {
-                deferred.reject();
-            }
-        }).on("error", function() {
-            deferred.reject();
-        }).attr("src", images[feature || "basic"]);
-        
-        return deferred.promise();
-    }
+	var images = {
+		basic : "data:image/webp;base64,UklGRjIAAABXRUJQVlA4ICYAAACyAgCdASoCAAEALmk0mk0iIiIiIgBoSygABc6zbAAA/v56QAAAAA==",
+		lossless : "data:image/webp;base64,UklGRh4AAABXRUJQVlA4TBEAAAAvAQAAAAfQ//73v/+BiOh/AAA="
+	};
+
+	return function(feature) {
+		var deferred = $.Deferred();
+
+		$("<img>").on("load", function() {
+			if (this.width === 2 && this.height === 1) {
+				deferred.resolve();
+			} else {
+				deferred.reject();
+			}
+		}).on("error", function() {
+			deferred.reject();
+		}).attr("src", images[feature || "basic"]);
+
+		return deferred.promise();
+	}
 })();
 
-$(document).ready(function(){
-	if(getCookie("WEBP_SUPPORT") == null){
-		hasWebP().then(function() {
-			setRootCookie("WEBP_SUPPORT", "true", 24*60*60*1000*30)
-		}, function() {
-			setRootCookie("WEBP_SUPPORT", "false", 24*60*60*1000*30);
-		});
-	}
-	getToReadMessageCount();
-	if(isGetToReadMessageCount){
-		setInterval(function(){
-			getToReadMessageCount();
-		}, 20000);
-	}
-	$("img.avatar").error(function(){
-		var me = $(this);
-		me.attr("src",staticSourcePrefix+"/imgs/guest_160.png")
-	});
-	
-	$(document).on("click","a[data-play]",function(){
-		var me = $(this);
-		var loading = $('<img src="'+staticSourcePrefix+'/imgs/loading-64.gif" class="img-responsive"/>');
-		me.parent().hide().after(loading);
-		var img = $(me.find("img")[0]);
-		img.attr("src",$(this).attr("data-play"));
-		var span = $(me.find("span")[0]);
-		img.bind("load",function(){
-			span.hide();
-			loading.remove();
-			me.parent().show();
-		});
-	}); 
-});
+$(document)
+		.ready(
+				function() {
+					if (getCookie("WEBP_SUPPORT") == null) {
+						hasWebP().then(
+								function() {
+									setRootCookie("WEBP_SUPPORT", "true", 24
+											* 60 * 60 * 1000 * 30)
+								},
+								function() {
+									setRootCookie("WEBP_SUPPORT", "false", 24
+											* 60 * 60 * 1000 * 30);
+								});
+					}
+					getToReadMessageCount();
+					if (isGetToReadMessageCount) {
+						setInterval(function() {
+							getToReadMessageCount();
+						}, 20000);
+					}
+					$("img.avatar").error(
+							function() {
+								var me = $(this);
+								me.attr("src", staticSourcePrefix
+										+ "/imgs/guest_160.png")
+							});
+
+					$(document)
+							.on(
+									"click",
+									"a[data-play]",
+									function() {
+										var me = $(this);
+										var loading = $('<img src="'
+												+ staticSourcePrefix
+												+ '/imgs/loading-64.gif" class="img-responsive"/>');
+										me.parent().hide().after(loading);
+										var img = $(me.find("img")[0]);
+										img.attr("src", $(this).attr(
+												"data-play"));
+										var span = $(me.find("span")[0]);
+										img.bind("load", function() {
+											span.hide();
+											loading.remove();
+											me.parent().show();
+										});
+									});
+				});
 
 var token = $("meta[name='_csrf']").attr("content");
 var header = $("meta[name='_csrf_header']").attr("content");
-if(token != null && header != null && token != "" && header != ""){
+if (token != null && header != null && token != "" && header != "") {
 	$(document).ajaxSend(function(e, xhr, options) {
 		xhr.setRequestHeader(header, token);
 	});
 }
 function getUrlParam(name) {
-    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)"); 
-    var r = window.location.search.substr(1).match(reg);  
-    if (r != null) return unescape(r[2]); return null; 
+	var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+	var r = window.location.search.substr(1).match(reg);
+	if (r != null)
+		return unescape(r[2]);
+	return null;
 }
 
-function inArray(str,array){
-	for(var i=0;i<array.length;i++){
-		if(str == array[i]){
+function inArray(str, array) {
+	for (var i = 0; i < array.length; i++) {
+		if (str == array[i]) {
 			return i;
 		}
 	}
 	return -1;
 }
 
-
-function post(url,data,success_fn,error_fn,complete_fn){
+function post(url, data, success_fn, error_fn, complete_fn) {
 	$.ajax({
-        type: "post",
-        url:  url,
-        data: JSON.stringify(data),
-        dataType: "json",
-        contentType : 'application/json',
-        success: success_fn,
-        error:error_fn,
-       	complete:complete_fn
-    });
+		type : "post",
+		url : url,
+		data : JSON.stringify(data),
+		dataType : "json",
+		contentType : 'application/json',
+		success : success_fn,
+		error : error_fn,
+		complete : complete_fn
+	});
 }
 
-function get(url,data,success_fn,error_fn,complete_fn){
+function get(url, data, success_fn, error_fn, complete_fn) {
 	$.ajax({
-        type: "get",
-        url:  url,
-        data: JSON.stringify(data),
-        dataType: "json",
-        contentType : 'application/json',
-        success: success_fn,
-        error:error_fn,
-       	complete:complete_fn
-    });
+		type : "get",
+		url : url,
+		data : JSON.stringify(data),
+		dataType : "json",
+		contentType : 'application/json',
+		success : success_fn,
+		error : error_fn,
+		complete : complete_fn
+	});
 }
 
-function setRootCookie(name,value,time){
+function setRootCookie(name, value, time) {
 	var exp = new Date();
-    exp.setTime(exp.getTime() +time);
-    document.cookie = name + "="+ escape (value) + ";expires=" + exp.toGMTString()+";path=/;domain="+getRootDomain();
+	exp.setTime(exp.getTime() + time);
+	document.cookie = name + "=" + escape(value) + ";expires="
+			+ exp.toGMTString() + ";path=/;domain=" + getRootDomain();
 }
-//读取cookies
-function getCookie(name)
-{
-    var arr,reg=new RegExp("(^| )"+name+"=([^;]*)(;|$)");
- 
-    if(arr=document.cookie.match(reg))
- 
-        return (arr[2]);
-    else
-        return null;
+// 读取cookies
+function getCookie(name) {
+	var arr, reg = new RegExp("(^| )" + name + "=([^;]*)(;|$)");
+
+	if (arr = document.cookie.match(reg))
+
+		return (arr[2]);
+	else
+		return null;
 }
--- src/main/webapp/static/plugins/ckeditor/plugins/mycode/plugin.js
+++ src/main/webapp/static/plugins/ckeditor/plugins/mycode/plugin.js
@@ -1,30 +1,6 @@
-function drewcodeModal(){
-	var code_dialogHtml = '<div class="modal fade" id="ckeditor-codeModal" tabindex="-1" role="dialog"';
-	code_dialogHtml += '	aria-labelledby="myModalLabel" aria-hidden="true"';
-	code_dialogHtml += '	data-backdrop="static">';
-	code_dialogHtml += '	<div class="modal-dialog modal-md">';
-	code_dialogHtml += '		<div class="modal-content">';
-	code_dialogHtml += '			<div class="modal-header">';
-	code_dialogHtml += '				<button type="button" class="close" data-dismiss="modal">';
-	code_dialogHtml += '					<span aria-hidden="true">&times;</span><span class="sr-only">Close</span>';
-	code_dialogHtml += '				</button>';
-	code_dialogHtml += '				<h4 class="modal-title" id="myModalLabel">代码</h4>';
-	code_dialogHtml += '			</div>';
-	code_dialogHtml += '			<div class="modal-body">';
-	code_dialogHtml += '         		<select id="ckeditor-code-select" class="form-control" ><option value="please choose">请选择</option><option value="codeTag">code标签</option><option value="code">代码</option></select>';
-	code_dialogHtml += '				<div id="ckeditor-code-container" style="margin-top:10px"></div>';
-	code_dialogHtml += '			</div>';
-	code_dialogHtml += '			<div class="modal-footer">';
-	code_dialogHtml += '				<button type="button" class="btn btn-default" data-dismiss="modal">关闭</button>';
-	code_dialogHtml += '				<button type="button" class="btn btn-default" id="ckeditor-code-btn">确认</button>';
-	code_dialogHtml += '			</div>';
-	code_dialogHtml += '		</div>';
-	code_dialogHtml += '	</div>';
-	code_dialogHtml += '</div>';
-	return code_dialogHtml;
-}
 CKEDITOR.plugins.add('mycode', {
 	init: function( editor ) {
+		var me = this;
 		editor.ui.addButton( 'Mycode', {
 			label: '代码',
 			command: 'mycode',
@@ -38,7 +14,7 @@ CKEDITOR.plugins.add('mycode', {
 					editor.insertHtml("<code>"+text+"</code>");
 				}else{
 					if($("#ckeditor-codeModal").length == 0){
-						$(drewcodeModal()).appendTo("body").modal("show");
+						$(me.model()).appendTo("body").modal("show");
 					}else{
 						$("#ckeditor-code-input").val("");
 						$("#ckeditor-code-container").html("");
@@ -58,7 +34,14 @@ CKEDITOR.plugins.add('mycode', {
 							container.html("<input type='text' class='form-control' id='ckeditor-code-input'/>");
 						}
 						if(type == "code"){
-							container.html("<textarea class='form-control' id='ckeditor-code-textarea' style='height:300px'></textarea>");
+							var languages = me.languages;
+							var html = "<select class='form-control' id='ckeditor-code-lan' style='margin-bottom:20px'>";
+							for(var i=0;i<languages.length;i++){
+								var lan = languages[i];
+								html += "<option value='"+lan+"'>"+lan+"</option>";
+							}
+							html += "</select>";
+							container.html(html+"<textarea class='form-control' id='ckeditor-code-textarea' style='height:300px'></textarea>");
 						}
 					});
 					btn.off("click");
@@ -82,15 +65,44 @@ CKEDITOR.plugins.add('mycode', {
 								$.messager.popup("请输入内容");
 								return ;
 							}
+							var lan = $("#ckeditor-code-lan").val();
 							editor.insertHtml("<pre class='pre-scrollable'>");
-							editor.insertText(code);
-							editor.insertHtml("</pre>");
+							editor.insertHtml("<code class='"+lan+"'>");
+							editor.insertText(""+code+"");
+							editor.insertHtml("</code>");
+							editor.insertHtml("</pre>")
 						}
 						$("#ckeditor-codeModal").modal("hide");
 					});
 				}
 			}
 		});
+	},
+	languages:["Apache","Bash","C#","C++","CSS","CoffeeScript","dts","Diff","HTML,XML","HTTP","ini","JSON","Java","JavaScript","MakeFile","Markdown","Nginx","Objective-C","PHP","Perl","Python","Ruby","SQL"],
+	model:function(){
+		var code_dialogHtml = '<div class="modal fade" id="ckeditor-codeModal" tabindex="-1" role="dialog"';
+		code_dialogHtml += '	aria-labelledby="myModalLabel" aria-hidden="true"';
+		code_dialogHtml += '	data-backdrop="static">';
+		code_dialogHtml += '	<div class="modal-dialog modal-md">';
+		code_dialogHtml += '		<div class="modal-content">';
+		code_dialogHtml += '			<div class="modal-header">';
+		code_dialogHtml += '				<button type="button" class="close" data-dismiss="modal">';
+		code_dialogHtml += '					<span aria-hidden="true">&times;</span><span class="sr-only">Close</span>';
+		code_dialogHtml += '				</button>';
+		code_dialogHtml += '				<h4 class="modal-title" id="myModalLabel">代码</h4>';
+		code_dialogHtml += '			</div>';
+		code_dialogHtml += '			<div class="modal-body">';
+		code_dialogHtml += '         		<select id="ckeditor-code-select" class="form-control" ><option value="please choose">请选择</option><option value="codeTag">code标签</option><option value="code">代码</option></select>';
+		code_dialogHtml += '				<div id="ckeditor-code-container" style="margin-top:10px"></div>';
+		code_dialogHtml += '			</div>';
+		code_dialogHtml += '			<div class="modal-footer">';
+		code_dialogHtml += '				<button type="button" class="btn btn-default" data-dismiss="modal">关闭</button>';
+		code_dialogHtml += '				<button type="button" class="btn btn-default" id="ckeditor-code-btn">确认</button>';
+		code_dialogHtml += '			</div>';
+		code_dialogHtml += '		</div>';
+		code_dialogHtml += '	</div>';
+		code_dialogHtml += '</div>';
+		return code_dialogHtml;
 	}
 });
 
