35648bd58851dba75307f80f83ff47aec64fb31a
mhlx <admin@qyh.me>
2016-02-03 23:23:20 +0800

    为博客增加了简单的markdown的支持，并且现在博客的htmlContentHandler分两次，再保存博客前会调用BlogConfig中的handler，加载博客时会调用afterHandler

--- update.sql
+++ update.sql
@@ -1,3 +1,6 @@
 UPDATE blog_user SET user_password = '$2a$10$eJ8pM6Zl9tltoO/aHu9FFuUI6RIBoJmC8q740jajSj1X.Wt0JDowq' WHERE user_name = 'test';
 INSERT INTO user_role(user_id,role_id)
-VALUES((SELECT id FROM blog_user WHERE user_name = 'test'),(SELECT id FROM role WHERE role_name = 'ROLE_SUPERVISOR'))
+VALUES((SELECT id FROM blog_user WHERE user_name = 'test'),(SELECT id FROM role WHERE role_name = 'ROLE_SUPERVISOR'));
+ALTER TABLE blog ADD COLUMN editor INT NOT NULL DEFAULT 0;
+ALTER TABLE temporary_blog ADD COLUMN editor INT NOT NULL DEFAULT 0;
+ALTER TABLE blog ADD COLUMN display MEDIUMTEXT NOT NULL;
